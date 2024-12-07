9969e466b4caf88cd7319ea08e1e7e3f8697e85f
chea-young <lchy0413@gmail.com>
2024-12-07 10:47:23 +0900

    Feature: Link urls and templates

--- django/mysite/common/views.py
+++ django/mysite/common/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+def index(request):
+    template = loader.get_template("main.html")
+    context = {
+        
+    } # template에 담을 데이터
+    return HttpResponse(template.render(context, request))
