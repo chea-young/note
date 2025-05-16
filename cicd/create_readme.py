d0dc5caf2e0e0ccd2d12a6dfa81926815a5d0c99
chea-young <63410509+chea-young@users.noreply.github.com>
2025-05-16 07:07:48 +0000

    Feature: convert issue to markdown (#2)

--- cicd/create_readme.py
+++ cicd/create_readme.py
@@ -2,7 +2,13 @@ import requests
 
 owner = 'chea-young'
 repo = 'note'
+TOKEN = ''
 url = f'https://api.github.com/repos/{owner}/{repo}/issues'
+
+headers = {
+    'Authorization': f'token {TOKEN}',
+    'Accept': 'application/vnd.github.v3+json'
+}
 
 
 
@@ -12,4 +18,24 @@ def get_issue(issue_num):
 
     return issue
 
-issue = get_issue(2)
+def issue_to_markdown(issue):
+    markdown = f"# {issue['title']}\n"
+    markdown += f"{issue['body']}\n\n"
+    markdown += f"**Status:** {issue['state']}\n"
+    markdown += f"**Created at:** {issue['created_at']}\n"
+    markdown += f"**Updated at:** {issue['updated_at']}\n"
+    markdown += f"**URL:** {issue['html_url']}\n"
+    markdown += '\n---\n'
+    return markdown
+
+def save_to_readme(issue_id, content):
+    with open(f'{issue_id}.md', 'w') as f:
+        f.write(content)
+
+def main(issue_no):
+    issue = issue = get_issue(issue_no);
+    markdown = issue_to_markdown(issue);
+    save_to_readme(issue_no, markdown)
+
+if __name__ == '__main__':
+    main(2)
